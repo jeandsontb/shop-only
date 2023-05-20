32c8670a6d058c596d6e8b32b763dea05e527b8e
jeandson <jeandsontb@gmail.com>
2023-05-19 21:40:22 -0300

    adjust application

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+const Text = styled.Text``;
+
+const App = () => {
+  return (
+    <Container>
+      <Text>Projeto React Native</Text>
+    </Container>
+  );
+};
+
+export default App;
